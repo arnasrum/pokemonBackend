fd73c9d32e99a7c7d4ef04193a206c021a0f5b61
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2024-09-10 17:26:47 +0200

    Began fetching damage relations

--- src/main/resources/database/fetchTypes.py
+++ src/main/resources/database/fetchTypes.py
@@ -1,15 +1,40 @@
 import requests
 import json
 
-pokedex = {}
 i = 1
-response = requests.get(f"https://pokeapi.co/api/v2/type")
-response = response.json()
+response = requests.get(f"https://pokeapi.co/api/v2/type/{i}")
 types = {}
-for index, type in enumerate(response['results']):
-    types[index + 1] = type['name']
+damageRelations = {}
+while True:
+    if response.status_code == 200:
+        typeData = {}
+        #relation = {}
+        data = response.json()
+        typeData['id'] = data['id']
+        typeData['name'] = data['name']
+        typeData['damage_class'] = data['move_damage_class']
+        for relation in data['damage_relations']:
+            #print(data['damage_relations'][relation])
+            if relation == "double_damage_to":
+                for type in data['damage_relations'][relation]:
+                    id = type['url'].split("/")[-2]
+                    damageRelations[f'{typeData['id']} {id}'] = 2.0
+            if relation == "half_damage_to":
+                for type in data['damage_relations'][relation]:
+                    id = type['url'].split("/")[-2]
+                    damageRelations[f'{typeData['id']} {id}'] = 0.5 
+            if relation == "no_damage_to":
+                for type in data['damage_relations'][relation]:
+                    id = type['url'].split("/")[-2]
+                    damageRelations[f'{typeData['id']} {id}'] = 0.0
+        types[i] = typeData
+        i += 1
+        response = requests.get(f"https://pokeapi.co/api/v2/type/{i}")
+    else:
+        break
+
 with open("types.json", "w") as file:
     json.dump(types, file)
 
-#with open("types.json", "w") as file:
-#    json.dump(pokedex, file)
+with open("damageRelations.json", "w") as file:
+    json.dump(damageRelations, file)
